afd274a7ed1fcffde532951931423c13389c2d93
Eddy on ppappsrv02 <ridleys@gmail.com>
2015-10-19 10:39:39 +0200

    aggiunto il contesto di test e 2.701

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -1,2 +1,7 @@
 docker build -t jetty7ppws .
-docker run --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
+if [ $? -eq 0 ]; then
+	echo OK, launching!
+	docker run --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
+else
+    echo FAIL
+fi
