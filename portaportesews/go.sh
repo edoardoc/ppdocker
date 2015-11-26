1716941e9947a4eaf267f3c5c1fb0b17be3af784
Eddy on ppappsrv02 <ridleys@gmail.com>
2015-11-26 19:12:43 +0100

    aggiunto il nome e tolto -d per test prima

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -1,7 +1,7 @@
 docker build -t jetty7ppws .
 if [ $? -eq 0 ]; then
-	echo OK, launching!
-	docker run --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
+	echo OK, launching! daemon, remove -d to check logs
+	docker run --name datapp --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
 else
     echo FAIL
 fi
