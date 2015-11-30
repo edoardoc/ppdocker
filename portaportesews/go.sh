ea58c6187eb71373bffbf1cf321e9020ecf2374f
Eddy on ppappsrv02 <ridleys@gmail.com>
2015-11-30 18:08:05 +0100

    ultimi

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -1,7 +1,9 @@
 docker build -t jetty7ppws .
 if [ $? -eq 0 ]; then
-	echo OK, launching!
-	docker run --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
+	docker stop datapp
+	docker rm datapp
+	echo OK, launching! daemon, remove -d to check logs
+	docker run -d --name datapp --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
 else
     echo FAIL
 fi
