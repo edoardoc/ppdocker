a7784d51d6afc6e343fd9bcf64d730e92c882a56
Eddy on ppappsrv02 <ridleys@gmail.com>
2016-01-28 09:37:32 +0100

    versione con pp1 e pp3, provo a togliere il war ed usa il virtualhost con nginx

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -3,7 +3,8 @@ if [ $? -eq 0 ]; then
 	echo OK, launching daemon!
 	docker stop datapp
 	docker rm datapp
-	docker run -d --name datapp --add-host pp20.prv.lan:194.242.228.81 --add-host pp12.prv.lan:194.242.228.82 -p 80:8080 jetty7ppws
+	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp1.prv.lan:194.242.228.82 \
+		-e VIRTUAL_HOST=data.portaportese.it -e VIRTUAL_PORT=8080 jetty7ppws
 else
     echo FAIL
 fi
