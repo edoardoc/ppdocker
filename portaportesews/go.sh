28ebf15a0e1edd261e881d2c6624a89d9e54010f
Eddy on ppappsrv02 <ridleys@gmail.com>
2016-02-04 16:42:15 +0100

    multiplex host e disabilitato CORS in nginx

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]; then
 	docker stop datapp
 	docker rm datapp
 	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp1.prv.lan:194.242.228.82 \
-		-e VIRTUAL_HOST=data.portaportese.it -e VIRTUAL_PORT=8080 jetty7ppws
+		-e VIRTUAL_HOST=ppappsrv03.inroma.roma.it -e VIRTUAL_PORT=8080 jetty7ppws
 else
     echo FAIL
 fi
