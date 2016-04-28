d5153880fbd7bbd7c144f6f799e908af6de57ead
Eddy on ppappsrv02 <ridleys@gmail.com>
2016-04-28 20:16:57 +0200

    istruzione con rem per salvare il dump della richiesta con nginx

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -3,7 +3,7 @@ if [ $? -eq 0 ]; then
 	echo OK, launching daemon!
 	docker stop datapp
 	docker rm datapp
-	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp1.prv.lan:194.242.228.82 \
+	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp4.prv.lan:194.242.228.81 --add-host pp1.prv.lan:194.242.228.82 \
 		-e VIRTUAL_HOST=ppappsrv03.inroma.roma.it -e VIRTUAL_PORT=8080 jetty7ppws
 else
     echo FAIL
