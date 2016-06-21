c5f7a964bc52c758bc399a5bbafcfcc9658044a4
Edoardo Ceccarelli <ridleys@gmail.com>
2016-06-22 00:32:29 +0300

    rc

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -3,8 +3,9 @@ if [ $? -eq 0 ]; then
 	echo OK, launching daemon!
 	docker stop datapp
 	docker rm datapp
-	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp1.prv.lan:194.242.228.82 \
-		-e VIRTUAL_HOST=ppappsrv03.inroma.roma.it -e VIRTUAL_PORT=8080 --link mypostfix jetty7ppws
+	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp4.prv.lan:194.242.228.83 \
+								--add-host pp1.prv.lan:194.242.232.16 \
+		-e VIRTUAL_HOST=data.portaportese.it -e VIRTUAL_PORT=8080 --link mypostfix jetty7ppws
 else
     echo FAIL
 fi
