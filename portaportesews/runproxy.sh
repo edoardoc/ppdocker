563bc62453fb2e052300ad45b9001c499fa8fd62
Eddy on ppappsrv03 <ridleys@gmail.com>
2016-07-01 16:50:08 +0200

    nginx non permetteva attachment grandi in upload

--- portaportesews/runproxy.sh
+++ portaportesews/runproxy.sh
@@ -1,3 +1,6 @@
 docker stop myproxy
 docker rm myproxy
-docker run -d --name myproxy -p 80:80 -v nginx-vhost.d:/etc/nginx/vhost.d:ro -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run -d --name myproxy -p 80:80 \
+			-v nginx-vhost.d:/etc/nginx/vhost.d:ro \
+			-v /var/run/docker.sock:/tmp/docker.sock:ro \
+			ppproxy	
