9f51c5c9a7de07ad558bd48f440ebf9071a3f8e8
Eddy on ppappsrv03 <ridleys@gmail.com>
2016-07-19 15:06:07 +0200

    pp200

--- portaportesews/go.sh
+++ portaportesews/go.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]; then
 	docker stop datapp
 	docker rm datapp
 	docker run -d --name datapp --add-host pp3.prv.lan:194.242.228.81 --add-host pp4.prv.lan:194.242.228.83 \
-								--add-host pp1.prv.lan:194.242.232.16 \
+								--add-host pp1.prv.lan:194.242.232.20 \
 		-e VIRTUAL_HOST=data.portaportese.it -e VIRTUAL_PORT=8080 --link mypostfix jetty7ppws
 else
     echo FAIL
