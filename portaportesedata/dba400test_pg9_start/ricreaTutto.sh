378b03193232ab65b556a060fbfbaa9378219edd
Eddy on ppappsrv02 <ridleys@gmail.com>
2016-07-31 22:11:25 +0200

    ricreatutto non cancella il db erroneamente

--- portaportesedata/dba400test_pg9_start/ricreaTutto.sh
+++ portaportesedata/dba400test_pg9_start/ricreaTutto.sh
@@ -4,9 +4,11 @@ docker build -t portaportese/dba400 .
 # questo lancia pg con il volume
 docker stop pp24.prv.lan
 docker rm pp24.prv.lan 
-rm -rf /home/dba400/*
-docker run -d -v /root:/pginput -v /home/dba400:/pg/data --name pp24.prv.lan -i -t -p 5432:5432 -e POSTGRES_PASSWORD=password portaportese/dba400 
 
+# ATTENZIONE !!!
+# rm -rf /home/dba400/*
+
+docker run -d -v /home/postgresdumps:/pginput -v /home/dba400:/pg/data --name pp24.prv.lan -i -t -p 5432:5432 -e POSTGRES_PASSWORD=password portaportese/dba400 
 
 # cmd da lanciare da altra shell
 #docker exec -i -t pp24.prv.lan su - postgres -c "pg_restore -v -d dba400L1 -Fc /pginput/dba400dump_20160406-1217 -U pepper | psql template1"
