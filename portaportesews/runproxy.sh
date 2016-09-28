c0791e7ad435beef1d72b4dd9e11cf9d8bc8db11
Edoardo Ceccarelli <ridleys@gmail.com>
2016-09-28 13:59:32 +0300

    maledetta barra

--- portaportesews/runproxy.sh
+++ portaportesews/runproxy.sh
@@ -1,7 +1,7 @@
 docker stop myproxy
 docker rm myproxy
-docker run -d --name myproxy -p 80:80 \
-			-v ~/cert:/etc/nginx/certs
-			-v nginx-vhost.d:/etc/nginx/vhost.d:ro \
+docker run -d --name myproxy -p 80:80 					\
+			-v ~/cert:/etc/nginx/certs 					\
+			-v nginx-vhost.d:/etc/nginx/vhost.d:ro 		\
 			-v /var/run/docker.sock:/tmp/docker.sock:ro \
 			ppproxy	
