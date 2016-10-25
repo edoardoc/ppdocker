4800b2c708619186372829a8d2e6712cd6c36c8b
Eddy on ppappsrv03 <ridleys@gmail.com>
2016-10-25 19:27:46 +0200

    finale con sftp su macchina host

--- portaportesews/runmyproxy.sh
+++ portaportesews/runmyproxy.sh
@@ -4,7 +4,7 @@ cd ..
 docker stop myproxy
 docker rm myproxy
 docker run -d --name myproxy -p 80:80 -p 443:443 \
-			-v /home/web:/web/static \
+			-v /home/rmdtmsoft:/web/static \
 			-v ~/cert:/etc/nginx/certs \
 			-v ~/ppdocker/portaportesews/nginx-vhost.d:/etc/nginx/vhost.d:ro \
 			-v /var/run/docker.sock:/tmp/docker.sock:ro \
