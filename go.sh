d0686729324f42fd9d9ecab25c5e511818a5e87a
Edoardo Ceccarelli <ridleys@gmail.com>
2016-11-02 11:58:14 +0200

    servizio web su 8001, curl per simulare virtualhost, ip locale in var

--- go.sh
+++ go.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # settaggi per test ppappsrv01
-export IP_GESTIONALE=194.242.228.82
-export IP_DBA1=194.242.228.33
-export IP_DBA2=194.242.228.33
+# export IP_GESTIONALE=194.242.228.82
+# export IP_DBA1=194.242.228.33
+# export IP_DBA2=194.242.228.33
+
+# ATTENZIONE, PER USARE LOCALHOST DEVI VERIFICARE QUALE E'
+# L'IP CORRENTE!!!
+myip="$(ipconfig getifaddr en0)"
+echo my ip: "${myip}"
+export IP_GESTIONALE=${myip}
+export IP_DBA1=${myip}
+export IP_DBA2=${myip}
 
 docker-compose build
 docker-compose up
+
+
+# TEST:
+# curl -H "Host: data.portaportese.it" localhost
+# curl -H "Host: www.portaportese.it" localhost
