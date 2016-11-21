7b2150258f6d837d55e954889b8989ccc5583fbc
Eddy on ppappsrv03 <ridleys@gmail.com>
2016-11-21 12:59:32 +0100

    docker in produzione RC1

--- go.sh
+++ go.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# settaggi per test ppappsrv01
-# export IP_GESTIONALE=194.242.228.82
-# export IP_DBA1=194.242.228.33
-# export IP_DBA2=194.242.228.33
+# settaggi per produzione
+export IP_GESTIONALE=194.242.232.20
+export IP_DBA1=194.242.232.21
+export IP_DBA2=194.242.232.22
 
 # ATTENZIONE, PER USARE LOCALHOST DEVI VERIFICARE QUALE E'
 # L'IP CORRENTE!!!
-myip="$(ipconfig getifaddr en0)"
-echo my ip: "${myip}"
-export IP_GESTIONALE=${myip}
-export IP_DBA1=${myip}
-export IP_DBA2=${myip}
+#myip="$(ipconfig getifaddr en0)"
+#echo my ip: "${myip}"
+#export IP_GESTIONALE=${myip}
+#export IP_DBA1=${myip}
+#export IP_DBA2=${myip}
 
 docker-compose build
 docker-compose up
