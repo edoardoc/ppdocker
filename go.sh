48dd7c57140dd13d86603a8278d30d1543178985
Eddy on ppappsrv03 <ridleys@gmail.com>
2017-01-13 12:13:45 +0100

    etichette ai servizi

--- go.sh
+++ go.sh
@@ -13,12 +13,16 @@ if [ "$1" == "test" ]; then
 	export IP_GESTIONALE=${myip}
 	export IP_DBA1=${myip}
 	export IP_DBA2=${myip}
+elif [ "$1" == "production" ]; then
+        echo ATTENZIONE DB PRODUZIONE 
+        export IP_GESTIONALE=194.242.232.20
+        export IP_DBA1=194.242.232.21
+        export IP_DBA2=194.242.232.22
 fi
 
-#docker-compose build
-#docker-compose pull
-#docker-compose down
-docker-compose up
+docker-compose build
+docker-compose pull
+docker-compose up -d
 
 # TEST:
 # curl -H "Host: data.portaportese.it" localhost
