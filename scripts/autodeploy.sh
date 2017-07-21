1d874382db719773e177818084882ceec8757214
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-21 10:31:36 +0300

    primo autodeploy su ocean

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-branchina="/master"
-cd /root/rpp
+branchina="/dev"
+cd ~/ppweb
 git fetch
 NLINEE="$(git --no-pager diff HEAD..origin$branchina --name-only )"
 VER=$(</home/ppjr/default-ear/default-war/.git/refs/heads$branchina)
@@ -9,7 +9,11 @@ if [ -z "$NLINEE" ]; then
 		exit;
 else
 		echo 'eseguo pull'
-		echo "PP1 sta ripartendo dopo deploy automatico. Commit on line: $branchina - $VER" | mail -s 'portaportese server startup' eddyce@me.com,rcaccamo0@gmail.com,bastianelli.portaportese@gmail.com,edoardo.c@tiscali.it
-		/root/restart.sh
+		cd ~/ppweb
+		git pull
+		echo "PPOCEAN01 sta ripartendo dopo deploy automatico. Commit on line: $branchina - $VER" | mail -s 'portaportese server startup' eddyce@me.com
+		#,rcaccamo0@gmail.com,bastianelli.portaportese@gmail.com,edoardo.c@tiscali.it
+		cd ~/ppdocker
+		docker-compose restart www
 fi
 cd -
--- scripts/restart.sh
+++
@@ -1,7 +0,0 @@
-/root/ppstop
-cd /root/rpp
-git pull
-# chown -R nobody.nobody /home/ppjr/
-cd /home/ppjr
-sh resetpp.sh
-/root/pplaunch
