47cf671f02ef88f29cfa8ee3021a73178520fba8
Edoardo <eddy@ppocean01.net>
2017-07-24 15:56:40 +0000

    autodeploy

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -3,7 +3,7 @@ branchina="/dev"
 cd ~/ppweb
 git fetch
 NLINEE="$(git --no-pager diff HEAD..origin$branchina --name-only )"
-VER=$(</home/ppjr/default-ear/default-war/.git/refs/heads$branchina)
+VER=$(<~/ppweb/.git/refs/heads$branchina)
 if [ -z "$NLINEE" ]; then
 		echo $NLINEE
 		exit;
@@ -11,8 +11,7 @@ else
 		echo 'eseguo pull'
 		cd ~/ppweb
 		git pull
-		echo "PPOCEAN01 sta ripartendo dopo deploy automatico. Commit on line: $branchina - $VER" | mail -s 'portaportese server startup' eddyce@me.com
-		#,rcaccamo0@gmail.com,bastianelli.portaportese@gmail.com,edoardo.c@tiscali.it
+		#echo "PPOCEAN01 sta ripartendo dopo deploy automatico. Commit on line: $branchina - $VER" | mail -s 'portaportese server startup' eddyce@me.com,rcaccamo0@gmail.com,bastianelli.portaportese@gmail.com,edoardo.c@tiscali.it
 		cd ~/ppdocker
 		docker-compose restart www
 fi
