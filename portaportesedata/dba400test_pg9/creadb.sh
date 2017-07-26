1caa784a2e8857d9901c5009bf8f35cc0dabaeca
Eddy on ppappsrv02 <ridleys@gmail.com>
2017-07-26 17:21:23 +0200

    il vecchio pp24, primo db frutto della conversione pg9

--- portaportesedata/dba400test_pg9/creadb.sh
+++
@@ -1,5 +0,0 @@
-gosu postgres postgres --single <<- EOSQL
-    CREATE USER pepper with password 'dbPasswordExample' SUPERUSER;
-    CREATE DATABASE dba400;
-    GRANT ALL PRIVILEGES ON DATABASE dba400 TO pepper;
-EOSQL
--- portaportesedata/dba400test_pg9_start/ricreaTutto.sh
+++ portaportesedata/dba400test_pg9_start/ricreaTutto.sh
@@ -6,12 +6,12 @@ docker stop pp24.prv.lan
 docker rm pp24.prv.lan 
 
 # ATTENZIONE !!!
-# rm -rf /home/dba400/*
+rm -rf /home/dba400/*
 
 docker run -d -v /home/postgresdumps:/pginput -v /home/dba400:/pg/data --name pp24.prv.lan -i -t -p 5432:5432 -e POSTGRES_PASSWORD=password portaportese/dba400 
 
 # cmd da lanciare da altra shell
-#docker exec -i -t pp24.prv.lan su - postgres -c "pg_restore -v -d dba400L1 -Fc /pginput/dba400dump_20160406-1217 -U pepper | psql template1"
+#docker exec -i -t pp24.prv.lan su - postgres -c "pg_restore -v -d dba400L1 -Fc /pginput/dba400dump_20161231-1127 -U pepper | psql template1"
 
 #cd ~/backups/tabellepp && pg_restore minibackupdba | recode iso-8859-1..u8 | psql --host 192.168.59.103 --port 5434 --username pepper --dbname dba400 && cd -
 
