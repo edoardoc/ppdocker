8e52beaa5e038cc1ce640edb3be1e0697dc5b4be
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-26 12:37:23 +0300

    pronti al primo dump 3

--- scripts/dump_dba400.sh
+++ scripts/dump_dba400.sh
@@ -1,7 +1,7 @@
 docker build  -t mydumper ./mydumper
 docker run -ti --rm \
     -v /mnt/volume-fra1-dumps/:/dump \
-    -e PREFIX=dba400
+    -e PREFIX=dba400 \
     -e PGDB=dba400L1 \
     -e PGPASSWORD=dbPasswordExample \
     -e PGHOST=194.242.232.21 \
--- scripts/dump_dbpp.sh
+++ scripts/dump_dbpp.sh
@@ -1,6 +1,7 @@
 docker build  -t mydumper ./mydumper
 docker run -ti --rm \
     -v /mnt/volume-fra1-dumps/:/dump \
+    -e PREFIX=dbpp \
     -e PGDB=portaportese \
     -e PGPASSWORD=dbPasswordExampleLong \
     -e PGHOST=194.242.232.200 \
