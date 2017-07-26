eea798af61e213a170f6e476b95bb428aa9cb4c2
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-26 12:34:17 +0300

    pronti al primo dump

--- scripts/dump_dba400.sh
+++ scripts/dump_dba400.sh
@@ -1,10 +1,9 @@
-echo starting!
 docker build  -t mydumper ./mydumper
 docker run -ti --rm \
-    -v /tmp:/dump \
+    -v /mnt/volume-fra1-dumps/:/dump \
     -e PGDB=dba400L1 \
     -e PGPASSWORD=dbPasswordExample \
     -e PGHOST=194.242.232.21 \
     -e PGUSER=pepper \
     -e PGPORT=5432 \
-    mydumper dump
+    mydumper
