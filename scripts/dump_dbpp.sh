0eb78f18452b55340a7c2bfcf3df12412c142a22
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-26 14:53:24 +0300

    pronti al primo dump 6

--- scripts/dump_dbpp.sh
+++ scripts/dump_dbpp.sh
@@ -3,7 +3,7 @@ docker run -ti --rm \
     -e PREFIX=dbpp \
     -e PGDB=portaportese \
     -e PGPASSWORD=dbPasswordExampleLong \
-    -e PGHOST=194.242.232.200 \
+    -e PGHOST=194.242.232.20 \
     -e PGUSER=pepper \
     -e PGPORT=5432 \
     mydumper
