7940c6e25ad5d456792e3a0e4a5c4623f503e059
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-27 20:44:46 +0300

    il db si chiama dba400L1 ma in maiuscolopush il

--- scripts/init_databases_from_dump.sh
+++ scripts/init_databases_from_dump.sh
@@ -3,10 +3,10 @@ read  -r -p "PREMI C PER CONTINUARE..." key
 if [ "$key" = 'C' ]; then
     echo '\n RIMUOVE TUTTO'
     docker-compose down
-    rm -fr Postgres/dbpp
-    rm -fr Postgres/dba400 
-    mkdir -p Postgres/dba400
-    mkdir -p Postgres/dbpp
+    rm -fr /mnt/volume-fra1-01/dbpp
+    rm -fr /mnt/volume-fra1-01/dba400 
+    mkdir -p /mnt/volume-fra1-01/dba400
+    mkdir -p /mnt/volume-fra1-01/dbpp
     docker-compose up $1 --build mydba400 mydbpp 
     echo "devi lanciare 'sh scripts/restore_dba400.sh dba400_20170726-1622' quando l'init e' terminato"
 else
