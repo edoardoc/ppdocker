c7ebc380a91783e496e37f98b0ef5dc13abb7b58
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-27 15:54:18 +0300

    entrambedbrestoressss

--- scripts/restore_dba400.sh
+++ scripts/restore_dba400.sh
@@ -1,5 +1,1 @@
-#pg_restore origindb.sql.bin | recode iso-8859-1..u8 | psql --dbname utf8converteddb
 docker exec -i -t mydba400 su - postgres -c "pg_restore /pginput/$1 | recode iso-8859-1..u8 | psql --dbname dba400L1" 
-
-#docker exec -i -t mydba400 su - postgres -c "pg_restore -v -d dba400L1 -Fc /pginput/$1 -U pepper | psql template1"
-
--- scripts/restore_dbpp.sh
+++ scripts/restore_dbpp.sh
@@ -1,1 +1,1 @@
-docker exec -i -t mydbpp su - postgres -c "pg_restore /pginput/$1 | recode iso-8859-1..u8 | psql template1"
+docker exec -i -t mydbpp su - postgres -c "pg_restore /pginput/$1 | recode iso-8859-1..u8 | psql --dbname portaportese"
