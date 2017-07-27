329caf0b5ddfa8b4aca58a41ffc5814f38ff3870
Edoardo Ceccarelli <ridleys@gmail.com>
2017-07-27 14:07:19 +0300

    versione buona

--- mydbs/dba400/creadba400.sh
+++
@@ -1,5 +0,0 @@
-gosu postgres postgres --single <<- EOSQL
-    CREATE USER pepper with password 'dbPasswordExample' SUPERUSER;
-    CREATE DATABASE DBA400L1;
-    GRANT ALL PRIVILEGES ON DATABASE DBA400L1 TO pepper;
-EOSQL
--- mydbs/dbpp/creadbpp.sh
+++
@@ -1,5 +0,0 @@
-gosu postgres postgres --single <<- EOSQL
-    CREATE USER pepper with password 'dbPasswordExampleLong' SUPERUSER;
-    CREATE DATABASE portaportese;
-    GRANT ALL PRIVILEGES ON DATABASE portaportese TO pepper;
-EOSQL
