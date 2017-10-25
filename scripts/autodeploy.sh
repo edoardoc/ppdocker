4a1ff1c652cec2c8722fee8587c138e2cac47bc1
Edoardo <eddy@ppocean01.net>
2017-10-25 15:34:11 +0200

    autodeploy con go.sh

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -13,6 +13,6 @@ else
 		git pull
 		#echo "PPOCEAN01 sta ripartendo dopo deploy automatico. Commit on line: $branchina - $VER" | mail -s 'portaportese server startup' eddyce@me.com,rcaccamo0@gmail.com,bastianelli.portaportese@gmail.com,edoardo.c@tiscali.it
 		cd ~/ppdocker
-		docker-compose restart www
+		sh go.sh
 fi
 cd -
