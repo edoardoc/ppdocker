d5a8b0d24287171795b69b2e93586d0b16d76971
Edoardo Ceccarelli <ridleys@gmail.com>
2018-11-14 00:06:14 +0200

    manca solo il db

--- utilities/go.sh
+++ utilities/go.sh
@@ -1,5 +1,5 @@
 --------------------------------
-export WHICH_COMMIT=eab39677d76a
+export WHICH_COMMIT=f7ed7e1a3811
 sh ./update.sh
 docker build --build-arg WHICH_COMMIT=${WHICH_COMMIT} -t java-app .
-docker run -e WHICH_COMMIT=${WHICH_COMMIT} -it java-app sh mediavacanze.sh
+docker run -e WHICH_COMMIT=${WHICH_COMMIT} -it java-app sh mediavacanze.sh dbpp ./pp.json
