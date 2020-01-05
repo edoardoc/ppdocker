30c896300691b8ab511fdec835e266a018fd6b47
Edoardo Ceccarelli <ridleys@gmail.com>
2020-01-05 19:15:42 +0200

    azzeratoScripts

--- scripts/dump_dba400.sh
+++
@@ -1,9 +0,0 @@
-docker run -ti --rm \
-    -v /mnt/volume-fra1-dumps/:/dump \
-    -e PREFIX=dba400 \
-    -e PGDB=dba400L1 \
-    -e PGPASSWORD=dbPasswordExample \
-    -e PGHOST=194.242.232.21 \
-    -e PGUSER=pepper \
-    -e PGPORT=5432 \
-    mydumper
--- scripts/dump_dbpp.sh
+++
@@ -1,9 +0,0 @@
-docker run -ti --rm \
-    -v /mnt/volume-fra1-dumps/:/dump \
-    -e PREFIX=dbpp \
-    -e PGDB=portaportese \
-    -e PGPASSWORD=dbPasswordExampleLong \
-    -e PGHOST=194.242.232.20 \
-    -e PGUSER=pepper \
-    -e PGPORT=5432 \
-    mydumper
