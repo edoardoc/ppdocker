a13cca80dc0b9b8850c4a7b9fb0723a0063a1df8
root <root@vm8524.seewebcloud.it>
2020-02-13 10:52:12 +0100

    passwordsss

--- scripts/dump_dbnew.sh
+++ scripts/dump_dbnew.sh
@@ -2,7 +2,7 @@ docker run -ti --rm \
     -v /mnt/volume-fra1-dumps/:/dump \
     -e PREFIX=portaporteseDb \
     -e PGDB=portaportese \
-    -e PGPASSWORD=portaporteseDbUserPassword \
+    -e PGPASSWORD=${PGSQL_PASSWORD} \
     -e PGHOST=vm8411.seewebcloud.it \
     -e PGUSER=portaporteseDbUser \
     -e PGPORT=5432 \
