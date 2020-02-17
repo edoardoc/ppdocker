4fe06a9d5e2faf92ac35014be590e23bd9fb29b4
root <root@vm8523.seewebcloud.it>
2020-02-17 12:21:12 +0100

    ultimo commit dopo migrazione

--- scripts/restore_dba400.sh
+++ scripts/restore_dba400.sh
@@ -1,1 +1,1 @@
-docker exec -t mydba400 su - postgres -c "pg_restore /pginput/$1 | recode iso-8859-1..u8 | psql --dbname dba400L1" 2>&1 & 
+docker exec -t mydba400 su - postgres -c "pg_restore /pginput/$1 | recode iso-8859-1..u8 | psql --dbname dba400L1" > /dev/null 2>&1 & 
