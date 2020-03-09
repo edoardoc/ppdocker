e3f0b094554f4c6f60681b771f9653b617a1346e
Edoardo Ceccarelli <ridleys@gmail.com>
2020-03-09 22:50:34 +0200

    tolti scripts inutili

--- scripts/ftp/gestionaleauto.sh
+++
@@ -1,2 +0,0 @@
-# utenti ftp
-mkdir -p /home/ftpusers/gestionaleauto && (echo ${FTP_PASSWORD1}; echo ${FTP_PASSWORD1}) | pure-pw useradd gestionaleauto -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /home/ftpusers/gestionaleauto
